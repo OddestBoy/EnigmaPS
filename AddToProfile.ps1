eadf49fdbf14adeadbe3fe3fd5a573bc7396bbcc
OddestBoy <oddboythefurst@gmail.com>
2025-05-10 12:38:09 +0100

    Update AddToProfile.ps1

--- AddToProfile.ps1
+++ AddToProfile.ps1
@@ -1,4 +1,4 @@
 Write-Output "set-alias -name `"Enigma`" -value `"$PSScriptRoot\EnigmaPS.ps1`"" | Out-File $profile -Append -Encoding UTF8
 Write-Host "Alias `"Engima`" set in PS Profile"
-Write-Host "Open Powershell and use command `"Enigma -h`""
+Write-Host "Open Powershell or Terminal and use command `"Enigma -h`""
 Start-sleep 30
